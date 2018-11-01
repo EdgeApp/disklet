452d193aafb3ff5ed279eaa95e693969a1bf3e4f
William Swanson <swansontec@gmail.com>
2018-11-01 14:20:10 -0700

    Fix Flow errors in React Native test

--- diskletTest.js
+++ diskletTest.js
@@ -1,36 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// @flow
 
 import React, { Component } from 'react'
 import { AppRegistry, StyleSheet, Text, View } from 'react-native'
 
-import { makeReactNativeFolder } from './lib/disklet.rn.js'
 import { testFolderNoData } from './test/test-helpers.js'
+import { makeReactNativeFolder } from './lib/disklet.rn.js'
 
-export default class disklet extends Component {
-  constructor (props) {
+type Props = {}
+type State = { status: string }
+
+export default class disklet extends Component<Props, State> {
+  constructor (props: Props) {
     super(props)
     this.state = {
       status: 'Testing...'
     }
   }
 
-  componentDidMount () {
-    async function test (t) {
-      try {
-        await testFolderNoData(folder)
-        console.log('Success')
-        t.setState({ status: 'Success' })
-      } catch (e) {
-        console.log(e)
-        t.setState({ status: 'Failed' })
-      }
+  async componentDidMount () {
+    try {
+      await testFolderNoData(folder)
+      console.log('Success')
+      this.setState({ status: 'Success' })
+    } catch (e) {
+      console.log(e)
+      this.setState({ status: 'Failed' })
     }
-
-    test(this)
   }
 
   render () {
