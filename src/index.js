4c3de371c07c8cc3da19d332f86378dfe13cf8d8
William Swanson <swansontec@gmail.com>
2018-11-01 15:05:56 -0700

    Add Flow types to the utility functions

--- src/index.js
+++ src/index.js
@@ -14,3 +14,26 @@ export { makeMemoryFolder } from './memoryFolder.js'
 export { makeNodeFolder } from './nodeFolder.js'
 export { makeReactNativeFolder } from './reactNativeFolder.js'
 export { makeUnionFolder } from './unionFolder.js'
+
+export type ArrayLike<T> =
+  | $ReadOnlyArray<T>
+  | {
+      +length: number,
+      +[n: number]: T
+    }
+
+export type DiskletFile = {
+  delete(): Promise<mixed>,
+  getData(): Promise<Uint8Array>,
+  getText(): Promise<string>,
+  setData(data: ArrayLike<number>): Promise<mixed>,
+  setText(text: string): Promise<mixed>
+}
+
+export type DiskletFolder = {
+  delete(): Promise<mixed>,
+  file(name: string): DiskletFile,
+  folder(name: string): DiskletFolder,
+  listFiles(): Promise<Array<string>>,
+  listFolders(): Promise<Array<string>>
+}
