ea6904c414f115a4afdd7abf50aa1f8555358963
William Swanson <swansontec@gmail.com>
2018-11-02 15:56:36 -0700

    Add Flow types to storage backends

--- src/backends/dummy.js
+++ src/backends/dummy.js
@@ -1,7 +1,11 @@
-export function makeNodeFolder (path) {
+// @flow
+
+import { type DiskletFolder } from '../index.js'
+
+export function makeNodeFolder (path: string): DiskletFolder {
   throw new Error('Not available on React Native')
 }
 
-export function makeReactNativeFolder (path) {
+export function makeReactNativeFolder (): DiskletFolder {
   throw new Error('Not available on node.js')
 }
