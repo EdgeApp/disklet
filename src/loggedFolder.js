7536564ad86baefb17d66d752c9af3e5963f1a77
William Swanson <swansontec@gmail.com>
2018-11-03 01:29:34 -0700

    Add Flow types to folder wrappers

--- src/loggedFolder.js
+++ src/loggedFolder.js
@@ -1,8 +1,31 @@
+// @flow
+
+import {
+  type ArrayLike,
+  type DiskletFile,
+  type DiskletFolder
+} from './index.js'
+
+type LogOperation =
+  | 'delete file'
+  | 'delete folder'
+  | 'get data'
+  | 'get text'
+  | 'list files'
+  | 'list folders'
+  | 'set data'
+  | 'set text'
+
+type LogOptions = {
+  callback?: (path: string, operation: LogOperation) => mixed,
+  verbose?: boolean
+}
+
 function logConsole (path, operation) {
   console.log(`${operation} "${path}"`)
 }
 
-function log (path, operation, opts) {
+function log (path, operation: LogOperation, opts) {
   const f = opts.callback != null ? opts.callback : logConsole
 
   if (opts.verbose || /set|delete/.test(operation)) {
@@ -11,56 +34,63 @@ function log (path, operation, opts) {
 }
 
 class LoggedFile {
-  constructor (file, path, opts) {
+  _file: DiskletFile
+  _opts: LogOptions
+  _path: string
+
+  constructor (file: DiskletFile, path: string, opts: LogOptions) {
     this._file = file
     this._path = path
     this._opts = opts
   }
 
-  delete () {
+  delete (): Promise<mixed> {
     log(this._path, 'delete file', this._opts)
     return this._file.delete()
   }
 
-  getData () {
+  getData (): Promise<Uint8Array> {
     log(this._path, 'get data', this._opts)
     return this._file.getData()
   }
 
-  getText () {
+  getText (): Promise<string> {
     log(this._path, 'get text', this._opts)
     return this._file.getText()
   }
 
-  setData (data) {
+  setData (data: ArrayLike<number>): Promise<mixed> {
     log(this._path, 'set data', this._opts)
     return this._file.setData(data)
   }
 
-  setText (text) {
+  setText (text: string): Promise<mixed> {
     log(this._path, 'set text', this._opts)
     return this._file.setText(text)
   }
 
-  getPath () {
-    log(this._path, 'get path', this._opts)
-    return this._file
+  getPath (): string {
+    return this._path
   }
 }
 
 class LoggedFolder {
-  constructor (folder, path, opts) {
+  _folder: DiskletFolder
+  _opts: LogOptions
+  _path: string
+
+  constructor (folder: DiskletFolder, path: string, opts: LogOptions) {
     this._folder = folder
     this._path = path
     this._opts = opts
   }
 
-  delete () {
+  delete (): Promise<mixed> {
     log(this._path, 'delete folder', this._opts)
     return this._folder.delete()
   }
 
-  file (name) {
+  file (name: string): DiskletFile {
     return new LoggedFile(
       this._folder.file(name),
       this._path + name,
@@ -68,7 +98,7 @@ class LoggedFolder {
     )
   }
 
-  folder (name) {
+  folder (name: string): DiskletFolder {
     return new LoggedFolder(
       this._folder.folder(name),
       this._path + name + '/',
@@ -76,17 +106,20 @@ class LoggedFolder {
     )
   }
 
-  listFiles () {
+  listFiles (): Promise<Array<string>> {
     log(this._path, 'list files', this._opts)
     return this._folder.listFiles()
   }
 
-  listFolders () {
+  listFolders (): Promise<Array<string>> {
     log(this._path, 'list folders', this._opts)
     return this._folder.listFolders()
   }
 }
 
-export function makeLoggedFolder (folder, opts = {}) {
+export function makeLoggedFolder (
+  folder: DiskletFolder,
+  opts: LogOptions = {}
+): DiskletFolder {
   return new LoggedFolder(folder, '', opts)
 }
