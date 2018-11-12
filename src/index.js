4ce08861d0651532ceba7381c5545c4608f132f3
William Swanson <swansontec@gmail.com>
2018-11-12 10:35:01 -0800

    Define the new `Disklet` API

--- src/index.js
+++ src/index.js
@@ -19,3 +19,21 @@ export type ArrayLike<T> =
       +length: number,
       +[n: number]: T
     }
+
+export type DiskletListing = { [path: string]: 'file' | 'folder' }
+
+export type Disklet = {
+  // Like `rm -r path`:
+  delete(path: string): Promise<mixed>,
+
+  // Like `cat path`:
+  getData(path: string): Promise<Uint8Array>,
+  getText(path: string): Promise<string>,
+
+  // Like `ls -l path`:
+  list(path?: string): Promise<DiskletListing>,
+
+  // Like `mkdir -p $(dirname path); echo data > path`:
+  setData(path: string, data: ArrayLike<number>): Promise<mixed>,
+  setText(path: string, text: string): Promise<mixed>
+}
