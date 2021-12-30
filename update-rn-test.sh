77eb489b3399dd036a8e49fea42e825af94e25ab
William Swanson <swansontec@gmail.com>
2021-12-30 10:10:39 -0800

    Fix build issues in the Android test app

--- update-rn-test.sh
+++ update-rn-test.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Copies the latest disklet code into the React Native test folder
+(
+  cd $(dirname $0)
 
-if [ ! -d ./ReactNativeDiskletTest/node_modules/disklet/ ]; then
-  echo 'Please run yarn inside the ReactNativeDiskletTest folder first'
-  exit 1
-fi
+  if [ ! -d ./ReactNativeDiskletTest/node_modules/disklet/ ]; then
+    echo 'Please run yarn inside the ReactNativeDiskletTest folder first'
+    exit 1
+  fi
 
-npm run prepare
+  npm run prepare
 
-rm -r ./ReactNativeDiskletTest/node_modules/disklet/lib/
-cp -r android disklet.podspec ios package.json lib ./ReactNativeDiskletTest/node_modules/disklet/
+  rm -r ./ReactNativeDiskletTest/node_modules/disklet/lib/
+  cp -r android disklet.podspec ios package.json lib ./ReactNativeDiskletTest/node_modules/disklet/
+)
